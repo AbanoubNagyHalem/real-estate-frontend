ffce230e83fcac98ff22c78fb6756e84f5eefb7a
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2025-05-27 14:48:52 +0300

    navbar + header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full overflow-hidden">
+      <Header />
+    </div>
+  );
 };
 
 export default App;

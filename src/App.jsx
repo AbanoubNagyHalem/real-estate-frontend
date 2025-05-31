845c0873518e98df158139c3c325c3294ecbb0a6
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2025-05-31 14:41:03 +0300

    Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import About from "./components/About";
 import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Testmonails from "./components/Testmonails";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
       <Header />
       <About />
+      <Projects />
+      <Testmonails />
     </div>
   );
 };
---
+++ src/components/Testmonails.jsx
@@ -0,0 +1,13 @@
+
+const Testmonails = () => {
+  return (
+    <div>
+      <h1>
+        Customer <span>Testmonials</span>
+        <p>Real Stories from Those Who Found Home with Us</p>
+      </h1>
+    </div>
+  );
+};
+
+export default Testmonails;

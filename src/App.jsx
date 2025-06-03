c8970ea0ea8a86fc0a01b10ebe2705e90935c6be
Abanoub Nagy <96650563+AbanoubNagyHalem@users.noreply.github.com>
2025-06-03 22:40:26 +0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Testmonails from "./components/Testmonails";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="w-full overflow-hidden">
+      <ToastContainer />
       <Header />
       <About />
       <Projects />
       <Testmonails />
       <Contact />
+      <Footer />
     </div>
   );
 };
